c55e827b72b04ab54432371c96cb50a2d1ff179b
Boidar <mity.grood1@gmail.com>
2024-11-08 18:07:48 +0300

    first commit

---
+++ src/main/java/Cat.java
@@ -0,0 +1,10 @@
+public class Cat {
+    private String tail;
+
+    public String getTail(){
+        return tail;
+    }
+    public void setTail(String tail) {
+        this.tail = tail;
+    }
+}
